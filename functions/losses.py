05374a71bb5f9dc34caaf8bd595c35e3ae75b3c6
klae01 <tspt2479@gmail.com>
2022-06-03 16:20:14 +0900

    basic design

--- functions/losses.py
+++ functions/losses.py
@@ -1,30 +1,40 @@
 import torch
+from . import utils
 
-
-def noise_estimation_loss(
+def model_loss_evaluation(
     model,
     x0: torch.Tensor,
-    t: torch.LongTensor,
     e: torch.Tensor,
-    a: torch.Tensor,
+    a_sqrt: torch.Tensor,
 ):
-    a = a.index_select(0, t).view(-1, 1, 1, 1)
-    x = x0 * a.sqrt() + e * (1.0 - a).sqrt()
-    y = model(x, t.long())
+    a = a_sqrt.square()
+    x0_lp = utils.log_polar_convert(x0)
+    e = utils.log_polar_noise_processing(e)
 
+    x = x0_lp * a_sqrt + e * (1 - a).sqrt()
+    y, sig_y = model(x, a_sqrt)
+
+    # scalar loss (NLL)
+    a = e[..., 0]
+    b = y[..., 0]
+    sig = sig_y[..., 0]
+    var = sig.square()
+    loss = torch.log1p(-torch.exp(-var/2) * torch.cos(a - b)).mean()
+
+    # angular loss (NLL direct)
+    a = e[..., 1]
+    b = y[..., 1]
+    sig = sig_y[..., 1] + 1e-3
+    loss += torch.log(sig).mean() + ((a-b)/sig).square().mean()/2
+    
     with torch.no_grad():
-        x_hat = x0 + (1 / a - 1).sqrt() * (e - y)
-        signal = x0.norm(dim=-3, p=2)
-        noise = x_hat.norm(dim=-3, p=2) - signal
+        x_hat = utils.log_polar_invert(x0_lp + (1 / a - 1).sqrt() * (e - y))
+
+        signal = x0.norm(dim=-1, p=2)
+        noise = x_hat.norm(dim=-1, p=2) - signal
         SNR = 10 * (
             signal.square().mean().clamp(min=1e-20).log10()
             - noise.square().mean().clamp(min=1e-20).log10()
         )
 
-    loss = (e - y).square().sum(dim=(1, 2, 3)).mean()
     return {"loss": loss, "SNR": SNR}
-
-
-loss_registry = {
-    "simple": noise_estimation_loss,
-}
--- functions/utils.py
+++ functions/utils.py
@@ -1,23 +1,29 @@
 import torch
 
 
-def conj2polar(x: torch.Tensor):
-    # x.shape = [*, 2, -1, -1]
+def log_polar_convert(x: torch.Tensor):
+    # x shape: [..., 2]
     scale = torch.linalg.norm(x, ord=2, axis=-3, keepdims=True)
+    log_scale = torch.log(scale + torch.exp(-16)) / 8 + 1
+    print(log_scale.min(), log_scale.max())
     return torch.concat(
-        [scale, torch.cos(x[..., 1:, :, :] / scale) * torch.sign(x[..., :1, :, :])],
-        axis=-2,
+        [log_scale, torch.acos(x[..., 0] / scale) * torch.sign(x[..., 1])],
+        axis=-1,
     )
 
-
-def polar2conj(x: torch.Tensor):
-    # x.shape = [*, 2, -1, -1]
-    scale = x[..., :1, :, :]
-    theta = x[..., 1:, :, :]
-    theta = [scale * torch.sin(theta), scale * torch.cos(theta)]
-
-    scale = torch.linalg.norm(x, ord=2, axis=-3, keepdims=True)
+def log_polar_invert(x: torch.Tensor):
+    # x shape: [..., 2]
+    log_scale = x[..., 0]
+    scale = torch.exp((log_scale-1)*8)
+    theta = x[..., 1]
     return torch.concat(
-        [scale, torch.cos(x[..., 1:, :, :] / scale) * torch.sign(x[..., :1, :, :])],
-        axis=-2,
+        [scale * torch.cos(theta), scale * torch.sin(theta)],
+        axis=-1,
     )
+
+def log_polar_noise_processing(x: torch.Tensor):
+    std, mean = torch.std_mean(x[..., 1])
+    x[..., 1].sub_(mean).div_(std)
+
+def log_polar_state_processing(x: torch.Tensor):
+    x[..., 1] = (x[..., 1]+torch.pi).fmod(torch.pi*2)-torch.pi
