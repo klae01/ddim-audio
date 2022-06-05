9a98d0a463243c1954cc03e4f294e180f32d392a
klae01 <tspt2479@gmail.com>
2022-06-05 21:10:49 +0900

    log polar as optional init

--- functions/denoising.py
+++ functions/denoising.py
@@ -14,7 +14,8 @@ def generalized_steps(
         if index != 0:
             x.mul_(a_s).add_(y, alpha=a_cs)
         alpha_push[...] = a_s
-        x = utils.angle_normalize(x)
+        if config:
+            x = utils.angle_normalize(x)
         y, sig_y = model(x, alpha_push)
         # x.addcmul_(torch.randn_like(mu_y), sig_y, value=-a_cs).div_(a_s)
         y.addcmul_(torch.randn_like(y), sig_y)
@@ -25,5 +26,5 @@ def generalized_steps(
             or index in select_index
             or index - num_steps in select_index
         ):
-            xs.append(utils.log_polar_invert(x, config).to("cpu"))
+            xs.append((utils.log_polar_invert(x, config) if config else x).to("cpu"))
     return xs
